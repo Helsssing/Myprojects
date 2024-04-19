c98747aa4a1f6b00fbf31a7fd87805e5805e993c
Helsssing <helsssing@mail.ru>
2024-04-19 18:56:20 +0300

    Строковый калькулятор разбит на методы

--- Projects/StrokeCalc/Main.java
+++ Projects/StrokeCalc/Main.java
@@ -6,9 +6,14 @@ public class Main {
     public static void main(String[] args) throws Exception {
         Scanner scan = new Scanner(System.in);
         String prim = scan.nextLine();
+        calc(prim);
+        print(calc(prim));
+
+    }
+
+    static String calc(String prim) throws Exception {
         char action;
         String[] data;
-
 
         if (prim.contains(" + ")) {
             data = prim.split(" \\+ ");
@@ -26,15 +31,12 @@ public class Main {
             throw new Exception("Некорректный знак ввода");
         }
 
-
-
         if (action == '*' || action == '/') {
             if (data[1].contains("\"")) throw new Exception("Строчку необходимо умножать или делить только на число");
         }
 
 
         if (!data[0].contains("\"")) throw new Exception("Строка без кавычек");
-
 
 
         for (int i = 0; i < data.length; i++) {
@@ -46,40 +48,45 @@ public class Main {
 
 
         if (action == '+') {
-            String result = data[0] + data[1];
-            System.out.println("\"" + result + "\"");
+            return data[0] + data[1];
+
 
         } else if (action == '*') {
             int multiplier = Integer.parseInt(data[1]);
             if (multiplier < 1 | multiplier > 10) throw new Exception("Вводимые числа возможны в диапазоне от 1 до 10");
-            String result = "";
-            for (int i = 0; i < multiplier; i++) {
-                result += data[0];
-            }if (result.length() > 40) {
-                String result1 = result.substring(0, 40);
-                System.out.println("\"" + result1 + "..." + "\"");
-            }else {
-                System.out.println("\"" + result + "\"");
-            }
+            return data[0].repeat(multiplier);
+
         } else if (action == '-') {
             int index = data[0].indexOf(data[1]);
             if (index == -1) {
-                System.out.println("\"" + data[0] + "\"");
+                return data[0];
             } else {
                 String result = data[0].substring(0, index);
                 result += data[0].substring(index + data[1].length());
-                System.out.println("\"" + result + "\"");
+                return result;
 
             }
         } else {
             int newLen = data[0].length() / Integer.parseInt(data[1]);
-            if (Integer.parseInt(data[1]) < 1 | Integer.parseInt(data[1]) > 10) throw new Exception("Вводимые числа возможны в диапазоне от 1 до 10");
-            String result = data[0].substring(0, newLen);
-            System.out.println("\"" + result + "\"");
+            if (Integer.parseInt(data[1]) < 1 | Integer.parseInt(data[1]) > 10)
+                throw new Exception("Вводимые числа возможны в диапазоне от 1 до 10");
+            return data[0].substring(0, newLen);
         }
+    }
 
+    static void print (String text){
+        if (text.length() > 40) {
+            String tooLong = text.substring(0, 40);
+            System.out.println("\"" + tooLong + "..." + "\"");
+        } else {
+            System.out.println("\"" + text + "\"");
+        }
     }
 
 }
+
+
+
+
 
 
